edad60f14fc2703ec9a5f59bd3f5ce3c3c4891c0
Sayan Karmakar <sayankarmakar09990@gmail.com>
2026-03-03 07:19:56 +0530

    FEAT : Build election apis

--- src/main/java/com/sayan/code/onlinevotingsystem/Service/ElectionServices.java
+++ src/main/java/com/sayan/code/onlinevotingsystem/Service/ElectionServices.java
@@ -1,4 +1,15 @@
 package com.sayan.code.onlinevotingsystem.Service;
 
+import com.sayan.code.onlinevotingsystem.Entity.Election;
+import com.sayan.code.onlinevotingsystem.Entity.VoteResult;
+
 public interface ElectionServices {
+    boolean addElection(Election election);
+    Election getElection(String id);
+    String closeElection(String id);
+    Election updateElection(Election election);
+    boolean publishElection(String id);
+    boolean generateResult(String cons_id, String admin_id);
+    boolean publishResult(String cons_id, String admin_id);
+
 }
