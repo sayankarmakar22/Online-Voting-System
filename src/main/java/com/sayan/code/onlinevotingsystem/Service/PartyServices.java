57d580533c71a0902dbb732e4c19560ec5950914
Sayan Karmakar <sayankarmakar09990@gmail.com>
2026-03-03 07:38:31 +0530

    FEAT : Build Party apis

--- src/main/java/com/sayan/code/onlinevotingsystem/Service/PartyServices.java
+++ src/main/java/com/sayan/code/onlinevotingsystem/Service/PartyServices.java
@@ -1,4 +1,10 @@
 package com.sayan.code.onlinevotingsystem.Service;
 
+import com.sayan.code.onlinevotingsystem.Entity.Party;
+
 public interface PartyServices {
+    String addParty(Party party);
+    String removeParty(String id);
+    String updateParty(Party party);
+    Party getParty(String id);
 }
